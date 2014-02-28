ece61a97a5c8b0b81c5a96b091d8c847ea799d50
Kevin Montgomery <bitbucket_kevin@kemosite.com>
2014-02-27 23:46:18 -0500

    February 27, 2014

--- geoiplite2/geoiplite2.php
+++ geoiplite2/geoiplite2.php
@@ -2,17 +2,19 @@
 
 /*
  * [MYSQL SAMPLE] *
- INSERT INTO  `geolite2`.`geolite2-city-blocks` (
+ INSERT INTO  `geoiplite2`.`geoiplite2-city-blocks` (
 `geoname_id` ,
 `continent_code`
 )
 VALUES (
-'0',  '0'
+'1',  '1'
+), (
+'2',  '2'
 );
  */
 $row = 0;
 
-if (($handle = fopen("GeoLite2-City-CSV\GeoLite2-City-CSV_20140204\GeoLite2-City-Locations.csv", "r")) !== FALSE) {
+if (($handle = fopen("GeoLite2-City-CSV\GeoLite2-City-CSV_20140204\GeoLite2-City-Blocks.csv", "r")) !== FALSE) {
 	
 	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 		
@@ -22,27 +24,42 @@ if (($handle = fopen("GeoLite2-City-CSV\GeoLite2-City-CSV_20140204\GeoLite2-City
 
 			$sql_stmt = 'INSERT INTO `geolite2`.`geolite2-city-blocks` (';
 
-			foreach($data as $key => $field) {
+			while (list($key, $field) = each($data)) {
+				
+				$sql_stmt .= '`'.$field.'`';
 
-				$sql_stmt .= '`'.$field.'`, ';
+				if (current($data)) {
+					$sql_stmt .= ', ';					
+				}
+
+			}
+
+			$sql_stmt .= ')'."\n";
+
+		} else {
+
+			/* [IF CONTINUING, SEPARATE VALUE LINES] */
+			if ($row > 1) { $sql_stmt .= ', '."\n"; }
+
+			// Insert Values...
+
+			$count = count($data) - 1;
+
+			$sql_stmt .= 'VALUES (';
+
+			while (list($key, $field) = each($data)) {
+
+				$sql_stmt .= "'".$field."'";
+
+				if ($key < $count) {
+					$sql_stmt .= ', ';					
+				}
 
 			}
 
 			$sql_stmt .= ')';
 
-			echo $sql_stmt;
-
-			echo '<pre>';
-			print_r($data);
-			echo '</pre>';
-
 		}
-
-		/*
-		echo '<pre>';
-		print_r($data);
-		echo '</pre>';
-		*/
 
 		$row++;
 
@@ -59,6 +76,10 @@ if (($handle = fopen("GeoLite2-City-CSV\GeoLite2-City-CSV_20140204\GeoLite2-City
     }
 
     fclose($handle);
+
+    echo "<pre>";
+    print_r($sql_stmt);
+    echo "</pre>";
 
 }
 
